179608d26c6d4b42fc5614e9ec70a58758977f18
BillQK <billngo47@gmail.com>
2021-08-19 00:00:39 +0700

    fix comments

--- BinarySearch/src/com/company/OrdArray.java
+++ BinarySearch/src/com/company/OrdArray.java
@@ -36,19 +36,19 @@ public class OrdArray {
         }
     }
 
-    public void insert(Long value) {
+    public void insert(Long value) {            // insert element into array
        int j;
        for (j = 0; j < nElems; j++)
        {
-           if (a[j] > value) break;
+           if (a[j] > value) break;             // return the index of the element is bigger than value
        }
        for (int k = nElems; k > j; k--)
        {
-           a[k] = a[k-1];
+           a[k] = a[k-1];                       // move all the upper elements +1 index
 
        }
-        a[j] = value;
-       nElems++;
+        a[j] = value;                           // add the value
+       nElems++;                                // increase the size
     }
 
     public boolean delete(Long value)              //delete element
@@ -58,22 +58,22 @@ public class OrdArray {
             if (a[j] == value) break;            // if the first element is
         }
         if (j == nElems) {
-            return false;
+            return false;                       // return false since j is greater than size -> Null Pointer Exception
         } else {
             for (int k = j; k < nElems; k++) {
-                a[k] = a[k + 1];
+                a[k] = a[k + 1];                // move all the upper element -1 index
             }
-            nElems--;
+            nElems--;                           // decrease the size
             return true;
         }
 
 
     }
 
-    public void display ()
+    public void display ()                      //display the array
     {
         for (int i = 0; i < nElems; i++) {
-            System.out.println(a[i] + " ");
+            System.out.print(a[i] + " ");
         }
     }
 }
