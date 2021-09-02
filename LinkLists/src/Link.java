adc1b769e633d5fb09e0367fa226649ae64b804f
BillQK <billngo47@gmail.com>
2021-09-02 15:50:52 +0700

    Simple LinkList

---
+++ LinkLists/src/Link.java
@@ -0,0 +1,16 @@
+public class Link {
+    public int iData;           // data item (key)
+    public double dData;        // data item
+    public Link next;           // next link in list
+
+    public Link(int id, double dd) {
+        iData =id;
+        dData = dd;
+    }
+
+    public void displayLink()
+    {
+        System.out.print("{" + iData + ", " + dData + "} ");
+    }
+}
+
